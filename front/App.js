f9354765db577804a73a8b3be2a865044c49cde6
robertovilla2102 <robertovilla22@outlook.com>
2020-03-18 18:32:40 -0300

    agregando view homePage

--- front/App.js
+++ front/App.js
@@ -3,7 +3,7 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
 
-import HomePage from './src/react/components/backgroundHome'
+import HomePage from './src/react/views/HomePage'
 
 export default class App extends Component {
   render() {
---
+++ front/src/redux/reducers/test-reducer.js
@@ -0,0 +1,11 @@
+
+const inicialState = {
+  test: {}
+}
+
+export default (state = inicialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+} 
