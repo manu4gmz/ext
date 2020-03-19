7b70ae971f4ee122f8e9c3aa1e8dc4582d52a4ee
robertovilla2102 <robertovilla22@outlook.com>
2020-03-19 10:07:07 -0300

    agregando funcionalidad navigation

--- front/App.js
+++ front/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
+
+//navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//Importando views and components
 import HomePage from './src/react/components/backgroundHome'
 import LoginPage from './src/react/components/backgroundLogin'
+
+const Stack = createStackNavigator();
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store} >
-        <HomePage />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomePage} />
+            <Stack.Screen name="Login" component={LoginPage} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider >
     );
   }
