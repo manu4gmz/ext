0a88e46e4ffb6bccad53ecc76985d7769c35c7d9
manu4gmz <manuel4gmz@gmail.com>
2020-03-30 17:28:07 -0300

    Mas o menos si das para atras y volves se mantienen algunas cosas

--- front/src/redux/reducers/index.js
+++ front/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import user from './user-reducer';
 import images from './image-reducer';
-import spaces from './spaces-reducer'
+import spaces from './spaces-reducer';
+import forms from './form-reducer';
 
 export default combineReducers({
   user,
   spaces,
-  images
+  images,
+  forms
 })
